abd451b1a263b6e3c6c74a36f9c3a009cafbb793
jay <shqinliang@corp.netease.com>
2020-03-16 21:10:23 +0800

    add graphql-server

---
+++ index.js
@@ -0,0 +1,40 @@
+const {ApolloServer, gql} = require('apollo-server')
+
+
+const typeDefs = gql`
+
+type Book{
+	title:String,
+	author:String
+}
+
+type Query{
+	books:[Book]
+}
+
+`;
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+const resolvers ={
+	Query:{
+		books:()=>books
+	}
+}
+
+
+
+const server = new ApolloServer({typeDefs,resolvers});
+
+server.listen().then(({url})=>{
+	console.log(`server ready ${url}`);
+})
